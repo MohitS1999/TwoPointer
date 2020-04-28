fd2e819f3f81644922b420a1d2949a8f0620eed8
MohitS1999 <49729753+MohitS1999@users.noreply.github.com>
2020-04-29 01:47:09 +0530

    Update TwoPointerAlgo.java

--- TwoPointerAlgo.java
+++ TwoPointerAlgo.java
@@ -7,8 +7,7 @@ public class TwoPointerAlgo{
 		for(int i=0;i<n;i++){
 			a[i]=sc.nextInt();
 		}
-		TwoPointerAlgo g=new TwoPointerAlgo();
-		if(g.findtriplet(a,n)){
+		if(findtriplet(a,n)){
 			System.out.println("1");
 		}
 		else{
@@ -16,14 +15,15 @@ public class TwoPointerAlgo{
 		}
 		
 	}
-	public boolean findtriplet(int a[],int n){
+	public static boolean findtriplet(int a[],int n){
+		//Sort the Array is compulsory fro two pointer Algorithm
 		Arrays.sort(a);
 		for(int i=0;i<n-2;i++){
 			if(twosum(a,-a[i],i+1)) return true;
 		}
 		return false;
 	}
-	public boolean twosum(int a[],int x,int i){
+	public static boolean twosum(int a[],int x,int i){
 		int j=a.length-1;
 		while(i<j){
 			if(a[i]+a[j]>x){
